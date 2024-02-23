eb6a19a539a6b0205507bb90901fb7558576b8ec
Joker <40009719+pic123123@users.noreply.github.com>
2024-02-23 09:56:32 +0900

    빅오소개 + 코드 시간 재기

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,9 +6,30 @@ export class AppService {
     return 'Hello World!';
   }
 
+  //#region Big O Notation
   @Timeout(1000)
-  async demo() {
+  demo() {
     console.log('HELLO FROM THE NODEJS!');
+
+    const t1 = performance.now();
+    //console.log(this.addUpto(6)); //21
+    console.log(this.addUpto(1000000000)); //500000000067109000
+    const t2 = performance.now();
+    console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
     return true;
   }
+
+  addUpto(n: number) {
+    // case1;
+    // let total = 0;
+    // for (let index = 0; index <= n; index++) {
+    //   total += index;
+    // }
+    // return total;
+
+    //case2
+    return (n * (n + 1)) / 2;
+  }
+
+  //#endregion
 }
